9d05aa7f2c1edd5362403a8b1911de8828cf5dc0
sebastianfula <sebastianfula@gmail.com>
2024-10-07 21:35:26 -0500

    ini: Commit inicial.

---
+++ src/main/java/com/uptc/fabrica/lafabricaapp/LaFabricaAppApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.fabrica.lafabricaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFabricaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaFabricaAppApplication.class, args);
+    }
+
+}
